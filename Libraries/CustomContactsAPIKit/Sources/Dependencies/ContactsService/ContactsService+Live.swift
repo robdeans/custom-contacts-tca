3d3f864b372ce4f26b1218bcc166e97b06ba2b0f
Rob Deans <deans@fueled.com>
2023-09-11 16:38:53 -0400

    chore(*): restructure DependencyValue files

---
+++ Libraries/CustomContactsAPIKit/Sources/Dependencies/ContactsService/ContactsService+Live.swift
@@ -0,0 +1,50 @@
+//
+//  ContactsService+Live.swift
+//  CustomContacts
+//
+//  Created by Robert Deans on 9/11/23.
+//  Copyright © 2023 RBD. All rights reserved.
+//
+
+import Contacts
+
+final class ContactsServiceLive: ContactsService {
+	private static let store = CNContactStore()
+	private static let keysToFetch: [Any] = [
+		CNContactGivenNameKey,
+		CNContactFamilyNameKey,
+		CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+	]
+
+	func fetchContacts() async throws -> [Contact] {
+		let request = CNContactFetchRequest(keysToFetch: Self.keysToFetch.compactMap { $0 as? CNKeyDescriptor })
+		return try await withCheckedThrowingContinuation { continuation in
+			do {
+				var contacts: [Contact] = []
+				try Self.store.enumerateContacts(with: request) { cnContact, _ in
+					contacts.append(Contact(cnContact))
+				}
+				continuation.resume(returning: contacts)
+			} catch {
+				continuation.resume(throwing: error)
+			}
+		}
+	}
+
+	func requestPermissions() async throws -> Bool {
+		switch CNContactStore.authorizationStatus(for: .contacts) {
+		case .authorized:
+			return true
+		case .notDetermined:
+			do {
+				return try await Self.store.requestAccess(for: .contacts)
+			} catch {
+				throw error
+			}
+		case .restricted, .denied:
+			return false
+		@unknown default:
+			return false
+		}
+	}
+}
--- Libraries/CustomContactsAPIKit/Sources/Dependencies/ContactsService/ContactsService.swift
+++ Libraries/CustomContactsAPIKit/Sources/Dependencies/ContactsService/ContactsService.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2023 RBD. All rights reserved.
 //
 
-import Contacts
 import Dependencies
 
 public protocol ContactsService {
@@ -16,63 +15,12 @@ public protocol ContactsService {
 
 private enum ContactsServiceKey: DependencyKey {
 	static let liveValue: ContactsService = ContactsServiceLive()
-	static let previewValue: ContactsService = ContactsServiceLive()
+	static let previewValue: ContactsService = ContactsServiceMock()
 }
 
 extension DependencyValues {
 	public var contactsService: ContactsService {
 		get { self[ContactsServiceKey.self] }
 		set { self[ContactsServiceKey.self] = newValue }
-	}
-}
-
-final class ContactsServiceLive: ContactsService {
-	private static let store = CNContactStore()
-	private static let keysToFetch: [Any] = [
-		CNContactGivenNameKey,
-		CNContactFamilyNameKey,
-		CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
-	]
-
-	func fetchContacts() async throws -> [Contact] {
-		let request = CNContactFetchRequest(keysToFetch: Self.keysToFetch.compactMap { $0 as? CNKeyDescriptor })
-		return try await withCheckedThrowingContinuation { continuation in
-			do {
-				var contacts: [Contact] = []
-				try Self.store.enumerateContacts(with: request) { cnContact, _ in
-					contacts.append(Contact(cnContact))
-				}
-				continuation.resume(returning: contacts)
-			} catch {
-				continuation.resume(throwing: error)
-			}
-		}
-	}
-
-	func requestPermissions() async throws -> Bool {
-		switch CNContactStore.authorizationStatus(for: .contacts) {
-		case .authorized:
-			return true
-		case .notDetermined:
-			do {
-				return try await Self.store.requestAccess(for: .contacts)
-			} catch {
-				throw error
-			}
-		case .restricted, .denied:
-			return false
-		@unknown default:
-			return false
-		}
-	}
-}
-
-final class ContactsServiceMock: ContactsService {
-	func fetchContacts() async throws -> [Contact] {
-		Contact.mockArray
-	}
-
-	func requestPermissions() async throws -> Bool {
-		true
 	}
 }
