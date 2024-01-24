bee2e7f10540c66d5aaa6b9d7e1e04850984cb68
Rob Deans <deans@fueled.com>
2024-01-24 16:19:17 -0500

    chore(test): add files to unit test

--- CustomContacts/Tests/CustomContactsTest.swift
+++ CustomContacts/Tests/CustomContactsTest.swift
@@ -6,23 +6,14 @@
 //  Copyright © 2023 RBD. All rights reserved.
 //
 
+import Dependencies
 import XCTest
 
 final class CustomContactsTest: XCTestCase {
+	func testLoadContacts() async {
+		let viewModel = ContactListView.ViewModel()
+		await viewModel.loadContacts(refresh: true)
+		XCTAssert(!viewModel.contacts.isEmpty)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-	func testExample() throws {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
-		// Any test you write for XCTest can be annotated as throws and async.
-		// Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-		// Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
 	}
 }
