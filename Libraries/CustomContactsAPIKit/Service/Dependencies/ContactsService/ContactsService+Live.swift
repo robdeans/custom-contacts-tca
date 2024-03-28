845056938f260f79499531fa75d18f7c2a764f0d
Rob Deans <deans@fueled.com>
2024-03-28 15:14:36 -0400

    refactor(contacts-service): introduce ContactStoreHandler actor to wrap CNContactStore

--- Libraries/CustomContactsAPIKit/Service/Dependencies/ContactsService/ContactsService+Live.swift
+++ Libraries/CustomContactsAPIKit/Service/Dependencies/ContactsService/ContactsService+Live.swift
@@ -10,46 +10,8 @@ import Contacts
 import CustomContactsHelpers
 import CustomContactsModels
 
-extension ContactsService {
-	public static var liveValue: Self {
-		Self(
-			fetchContacts: {
-				let request = CNContactFetchRequest(keysToFetch: Self.keysToFetch.compactMap { $0 as? CNKeyDescriptor })
-				return try await withCheckedThrowingContinuation { continuation in
-					do {
-						var contacts: [Contact] = []
-						let store = CNContactStore()
-						try store.enumerateContacts(with: request) { cnContact, _ in
-							contacts.append(Contact(cnContact))
-						}
-						LogInfo("Service returning \(contacts.count) contact(s)")
-						continuation.resume(returning: contacts)
-					} catch {
-						continuation.resume(throwing: error)
-					}
-				}
-			},
-			requestPermissions: {
-				let contactsAuthorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
-				switch contactsAuthorizationStatus {
-				case .authorized:
-					return true
-				case .notDetermined:
-					do {
-						let store = CNContactStore()
-						return try await store.requestAccess(for: .contacts)
-					} catch {
-						throw error
-					}
-				case .restricted, .denied:
-					return false
-				@unknown default:
-					LogWarning("Unknown Contact permissions case: \(contactsAuthorizationStatus)")
-					return false
-				}
-			}
-		)
-	}
+private actor ContactStoreHandler {
+	private lazy var store = CNContactStore()
 
 	private static var keysToFetch: [Any] {
 		[
@@ -63,5 +25,55 @@ extension ContactsService {
 			CNContactPostalAddressesKey,
 			CNContactPhoneNumbersKey,
 		]
+	}
+
+	func fetchContacts() async throws -> [Contact] {
+		let request = CNContactFetchRequest(keysToFetch: Self.keysToFetch.compactMap { $0 as? CNKeyDescriptor })
+		return try await withCheckedThrowingContinuation { continuation in
+			do {
+				var contacts: [Contact] = []
+				try store.enumerateContacts(with: request) { cnContact, _ in
+					contacts.append(Contact(cnContact))
+				}
+				LogInfo("Service returning \(contacts.count) contact(s)")
+				continuation.resume(returning: contacts)
+			} catch {
+				continuation.resume(throwing: error)
+			}
+		}
+	}
+	func requestPermissions() async throws -> Bool {
+		let contactsAuthorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
+		switch contactsAuthorizationStatus {
+		case .authorized:
+			return true
+		case .notDetermined:
+			do {
+				return try await store.requestAccess(for: .contacts)
+			} catch {
+				throw error
+			}
+		case .restricted, .denied:
+			return false
+		@unknown default:
+			LogWarning("Unknown Contact permissions case: \(contactsAuthorizationStatus)")
+			return false
+		}
+	}
+}
+
+extension ContactsService {
+	// Possible improvement for syncing:
+	// https://developer.apple.com/documentation/foundation/nsnotification/name/1403253-cncontactstoredidchange
+	public static var liveValue: Self {
+		let contactsHandler = ContactStoreHandler()
+		return Self(
+			fetchContacts: {
+				try await contactsHandler.fetchContacts()
+			},
+			requestPermissions: {
+				try await contactsHandler.requestPermissions()
+			}
+		)
 	}
 }
